ac85e3619893e9d65bb148505625d5388c65005a
DinuchDev <dinuch.sen@gmail.com>
2025-03-11 17:13:41 +0700

    feat item

--- lib/app/modules/inventory/items/controllers/items_controller.dart
+++ lib/app/modules/inventory/items/controllers/items_controller.dart
@@ -1,11 +1,34 @@
 import 'package:get/get.dart';
 
 class ItemsController extends GetxController {
-  //TODO: Implement ItemsController
+  var items = [
+    {
+      "title": "Item Groups",
+      "desc": "Create multiple variants of the same item using Item Groups",
+      "button": "New Item Group",
+      "image": "assets/images/avatar-profile.png"
+    },
+    {
+      "title": "Items",
+      "desc": "Create standalone items and services that you buy and sell",
+      "button": "New Item",
+      "image": "assets/images/avatar-profile.png"
+    },
+    {
+      "title": "Composite Items",
+      "desc": "Group different items together and sell them as a single item",
+      "button": "Enable Now",
+      "image": "assets/images/avatar-profile.png"
+    },
+    {
+      "title": "Price Lists",
+      "desc": "Tweak your item prices for specific contacts or transactions",
+      "button": "Enable Now",
+      "image": "assets/images/avatar-profile.png"
+    },
+  ].obs;
 
   final count = 0.obs;
-
-
 
   void increment() => count.value++;
 }
--- lib/app/modules/inventory/items/views/items_view.dart
+++ lib/app/modules/inventory/items/views/items_view.dart
@@ -1,22 +1,70 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
-
 import '../controllers/items_controller.dart';
 
 class ItemsView extends GetView<ItemsController> {
   const ItemsView({super.key});
+
   @override
   Widget build(BuildContext context) {
+    final ItemsController controller = Get.put(ItemsController());
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('ItemsView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'ItemsView is working',
-          style: TextStyle(fontSize: 20),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SizedBox(
+          height: Get.height,
+          child: Obx(
+            () => GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 16,
+                mainAxisSpacing: 16,
+                childAspectRatio: 1.8,
+              ),
+              itemCount: controller.items.length,
+              itemBuilder: (BuildContext context, int index) {
+                var item = controller.items[index];
+                return Card(
+                  elevation: 4,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        item["title"]!,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(height: 12),
+                      Image.asset(
+                        item["image"]!,
+                        width: 80,
+                        height: 80,
+                      ),
+                      SizedBox(height: 12),
+                      Text(
+                        item["desc"]!,
+                        textAlign: TextAlign.center,
+                      ),
+                      SizedBox(height: 15),
+                      ElevatedButton(
+                        onPressed: () {
+                          Get.snackbar(
+                            "Action",
+                            "${item["button"]} clicked",
+                            snackPosition: SnackPosition.BOTTOM,
+                          );
+                        },
+                        child: Text(item["button"]!),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
         ),
       ),
     );
