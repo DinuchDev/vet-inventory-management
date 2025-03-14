45a4adf0bd55a25ea3a601e1b9813293d48db975
DinuchDev <dinuch.sen@gmail.com>
2025-03-14 09:20:42 +0700

    feat: customer

--- lib/app/modules/sales/customers/views/customers_view.dart
+++ lib/app/modules/sales/customers/views/customers_view.dart
@@ -6,18 +6,45 @@ import '../controllers/customers_controller.dart';
 
 class CustomersView extends GetView<CustomersController> {
   const CustomersView({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('CustomersView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'CustomersView is working',
-          style: TextStyle(fontSize: 20),
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'Business is no fun without people.',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.w900),
+          ),
+          const Text(
+            'Create your image your contacts, all in one place.',
+          ),
+          TextButton(
+            onPressed: () {},
+            style: TextButton.styleFrom(backgroundColor: Colors.blue),
+            child: const Text(
+              'CREATE NEW CUSTOMER',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+          const Text(
+            'Click here to import customers form file',
+            style: TextStyle(fontSize: 12, color: Colors.blue),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Or using',
+                style: TextStyle(fontSize: 12),
+              ),
+              Icon(Icons.attachment),
+              Icon(Icons.attachment),
+              Icon(Icons.attachment),
+            ],
+          )
+        ],
       ),
     );
   }
