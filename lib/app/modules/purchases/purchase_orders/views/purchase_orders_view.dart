52178501cbab376e6e18f31778979936553485e3
DinuchDev <dinuch.sen@gmail.com>
2025-03-17 15:56:09 +0700

    feat(vendor & purchase_order: update and create)

--- lib/app/modules/purchases/purchase_orders/views/purchase_orders_view.dart
+++ lib/app/modules/purchases/purchase_orders/views/purchase_orders_view.dart
@@ -6,17 +6,94 @@ import '../controllers/purchase_orders_controller.dart';
 
 class PurchaseOrdersView extends GetView<PurchaseOrdersController> {
   const PurchaseOrdersView({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('PurchaseOrdersView'),
-        centerTitle: true,
+        automaticallyImplyLeading: false,
+        title: Row(
+          children: [
+            Text(
+              'All Purchase Orders',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            Icon(
+              Icons.keyboard_arrow_down,
+              color: Colors.blue,
+            )
+          ],
+        ),
+        actions: [
+          Row(
+            children: [
+              TextButton(
+                onPressed: () {},
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.blue.withOpacity(0.3),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(6), // Adjust the radius
+                  ),
+                ),
+                child: Row(
+                  children: [
+                    Icon(Icons.login_outlined, color: Colors.blue),
+                    SizedBox(width: 4.0),
+                    const Text(
+                      'In Transit Receives',
+                      style: TextStyle(color: Colors.blue),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(width: 8.0),
+              TextButton(
+                onPressed: () {},
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(6), // Adjust the radius
+                  ),
+                ),
+                child: Row(
+                  children: [
+                    Icon(Icons.add, color: Colors.white),
+                    const Text(
+                      'NEW',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          )
+        ],
       ),
-      body: const Center(
-        child: Text(
-          'PurchaseOrdersView is working',
-          style: TextStyle(fontSize: 20),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Start Managing Your Purchase Activities!',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.w900),
+            ),
+            const Text(
+              'Create, customize and send professional Purchase Orders to your vendors.',
+            ),
+            TextButton(
+              onPressed: () {},
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.blue,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(6), // Adjust the radius
+                ),
+              ),
+              child: const Text(
+                'CREATE NEW PURCHASE ORDER',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ],
         ),
       ),
     );
--- lib/app/modules/purchases/vendors/views/vendors_view.dart
+++ lib/app/modules/purchases/vendors/views/vendors_view.dart
@@ -10,86 +10,87 @@ class VendorsView extends GetView<VendorsController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          automaticallyImplyLeading: false,
-          title: Row(
-            children: [
-              Text(
-                'All Purchase Orders',
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-              Icon(
-                Icons.keyboard_arrow_down,
-                color: Colors.blue,
-              )
-            ],
-          ),
-          // actions: [
-          //   Row(
-          //     children: [
-          //       TextButton(
-          //         onPressed: () {},
-          //         style: TextButton.styleFrom(
-          //           backgroundColor: Colors.blue,
-          //           shape: RoundedRectangleBorder(
-          //             borderRadius: BorderRadius.circular(6), // Adjust the radius
-          //           ),
-          //         ),
-          //         child: Row(
-          //           children: [
-          //             Icon(Icons.add, color: Colors.white),
-          //             const Text(
-          //               'NEW',
-          //               style: TextStyle(color: Colors.white),
-          //             ),
-          //           ],
-          //         ),
-          //       ),
-          //     ],
-          //   )
-          // ],
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: Row(
+          children: [
+            Text(
+              'All Vendors',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            Icon(
+              Icons.keyboard_arrow_down,
+              color: Colors.blue,
+            )
+          ],
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                'Business is no fun without people.',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w900),
-              ),
-              const Text(
-                'Create and manage your contacts. all in one place.',
-              ),
-              TextButton(
-                onPressed: () {},
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.blue,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(6), // Adjust the radius
-                  ),
+        // actions: [
+        //   Row(
+        //     children: [
+        //       TextButton(
+        //         onPressed: () {},
+        //         style: TextButton.styleFrom(
+        //           backgroundColor: Colors.blue,
+        //           shape: RoundedRectangleBorder(
+        //             borderRadius: BorderRadius.circular(6), // Adjust the radius
+        //           ),
+        //         ),
+        //         child: Row(
+        //           children: [
+        //             Icon(Icons.add, color: Colors.white),
+        //             const Text(
+        //               'NEW',
+        //               style: TextStyle(color: Colors.white),
+        //             ),
+        //           ],
+        //         ),
+        //       ),
+        //     ],
+        //   )
+        // ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Business is no fun without people.',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.w900),
+            ),
+            const Text(
+              'Create and manage your contacts. all in one place.',
+            ),
+            TextButton(
+              onPressed: () {},
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.blue,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(6), // Adjust the radius
                 ),
-                child: const Text(
-                  'CREATE NEW VENDOR',
-                  style: TextStyle(color: Colors.white),
+              ),
+              child: const Text(
+                'CREATE NEW VENDOR',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+            Text(
+              'Click here to import vendor from files',
+              style: TextStyle(color: Colors.blue, fontSize: 12),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'or using',
+                  style: TextStyle(fontSize: 12),
                 ),
-              ),
-              Text(
-                'Click here to import vendor from files',
-                style: TextStyle(color: Colors.blue, fontSize: 12),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'or using',
-                    style: TextStyle(fontSize: 12),
-                  ),
-                  SizedBox(width: 8.0),
-                  Icon(Icons.attachment_outlined),
-                ],
-              )
-            ],
-          ),
-        ));
+                SizedBox(width: 8.0),
+                Icon(Icons.attachment_outlined),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
