cb48f7cb7f45ad3060ea5dce88f2568bd6715afd
DinuchDev <dinuch.sen@gmail.com>
2025-03-17 14:34:13 +0700

    feat: credit_notes

--- lib/app/modules/sales/credit_notes/views/credit_notes_view.dart
+++ lib/app/modules/sales/credit_notes/views/credit_notes_view.dart
@@ -6,19 +6,68 @@ import '../controllers/credit_notes_controller.dart';
 
 class CreditNotesView extends GetView<CreditNotesController> {
   const CreditNotesView({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('CreditNotesView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'CreditNotesView is working',
-          style: TextStyle(fontSize: 20),
-        ),
-      ),
-    );
+        appBar: AppBar(
+            automaticallyImplyLeading: false,
+            title: Row(
+              mainAxisSize: MainAxisSize.min,
+              // Prevents it from taking full width
+              children: const [
+                Text('All Credit Notes'),
+                Icon(Icons.keyboard_arrow_down, color: Colors.blue),
+              ],
+            ),
+            actions: [
+              Row(
+                children: [
+                  TextButton(
+                    onPressed: () {},
+                    style: TextButton.styleFrom(
+                      backgroundColor: Colors.blue,
+                      shape: RoundedRectangleBorder(
+                        borderRadius:
+                            BorderRadius.circular(6), // Adjust the radius
+                      ),
+                    ),
+                    child: Row(
+                      children: [
+                        Icon(Icons.add, color: Colors.white),
+                        const Text(
+                          'NEW',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              )
+            ]),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton(
+                onPressed: () {},
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(6), // Adjust the radius
+                  ),
+                ),
+                child: const Text(
+                  'CREATE NEW CREDIT NOTE',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              Text(
+                'Import Credit Notes',
+                style: TextStyle(fontSize: 12, color: Colors.blue),
+              )
+            ],
+          ),
+        ));
   }
 }
