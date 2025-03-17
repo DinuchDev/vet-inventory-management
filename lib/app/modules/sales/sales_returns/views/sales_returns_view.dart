5dcce6de57bc30985ffee3ebf28e9c9d13dceb85
DinuchDev <dinuch.sen@gmail.com>
2025-03-17 14:27:25 +0700

    feat: sales returns

--- lib/app/modules/sales/sales_returns/views/sales_returns_view.dart
+++ lib/app/modules/sales/sales_returns/views/sales_returns_view.dart
@@ -6,19 +6,38 @@ import '../controllers/sales_returns_controller.dart';
 
 class SalesReturnsView extends GetView<SalesReturnsController> {
   const SalesReturnsView({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('SalesReturnsView'),
-        centerTitle: true,
+        body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'Sales Returns',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.w900),
+          ),
+          const Text(
+              'Process your product returns in a few sample steps and get your inventory automatically sorted out.'),
+          const Text(
+            'Start creating sales return from sales orders',
+          ),
+          TextButton(
+            onPressed: () {},
+            style: TextButton.styleFrom(
+              backgroundColor: Colors.blue,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(6), // Adjust the radius
+              ),
+            ),
+            child: const Text(
+              'GO TO SALES ORDERS',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ],
       ),
-      body: const Center(
-        child: Text(
-          'SalesReturnsView is working',
-          style: TextStyle(fontSize: 20),
-        ),
-      ),
-    );
+    ));
   }
 }
