8d3eb4bbab8180f55993211fac758ce89d0af894
DinuchDev <dinuch.sen@gmail.com>
2025-03-17 14:17:39 +0700

    feat: payments_received

--- lib/app/modules/sales/payments_received/views/payments_received_view.dart
+++ lib/app/modules/sales/payments_received/views/payments_received_view.dart
@@ -6,19 +6,79 @@ import '../controllers/payments_received_controller.dart';
 
 class PaymentsReceivedView extends GetView<PaymentsReceivedController> {
   const PaymentsReceivedView({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('PaymentsReceivedView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'PaymentsReceivedView is working',
-          style: TextStyle(fontSize: 20),
-        ),
-      ),
-    );
+        appBar: AppBar(
+            automaticallyImplyLeading: false,
+            title: Row(
+              children: [
+                Text(
+                  'All Received Payments',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                Icon(
+                  Icons.keyboard_arrow_down,
+                  color: Colors.blue,
+                )
+              ],
+            ),
+            actions: [
+              Row(
+                children: [
+                  TextButton(
+                    onPressed: () {},
+                    style: TextButton.styleFrom(
+                      backgroundColor: Colors.blue,
+                      shape: RoundedRectangleBorder(
+                        borderRadius:
+                            BorderRadius.circular(6), // Adjust the radius
+                      ),
+                    ),
+                    child: Row(
+                      children: [
+                        Icon(Icons.add, color: Colors.white),
+                        const Text(
+                          'NEW',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              )
+            ]),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'No payments received, yet',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w900),
+              ),
+              const Text(
+                'Payments will be added once your customer pay for their invoices',
+              ),
+              TextButton(
+                onPressed: () {},
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(6), // Adjust the radius
+                  ),
+                ),
+                child: const Text(
+                  'GO TO UNPAID INVOICES',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              Text(
+                'Import Payments',
+                style: TextStyle(color: Colors.blue, fontSize: 12),
+              )
+            ],
+          ),
+        ));
   }
 }
