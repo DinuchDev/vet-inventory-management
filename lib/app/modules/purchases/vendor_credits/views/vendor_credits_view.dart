6901a6ff6bfd50da5eab1607f4ce516c6cda453c
DinuchDev <dinuch.sen@gmail.com>
2025-03-18 10:36:29 +0700

    feat(purchases):vendor_credits

--- lib/app/modules/purchases/vendor_credits/views/vendor_credits_view.dart
+++ lib/app/modules/purchases/vendor_credits/views/vendor_credits_view.dart
@@ -6,17 +6,73 @@ import '../controllers/vendor_credits_controller.dart';
 
 class VendorCreditsView extends GetView<VendorCreditsController> {
   const VendorCreditsView({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('VendorCreditsView'),
-        centerTitle: true,
+        automaticallyImplyLeading: false,
+        title: Row(
+          children: [
+            Text('All Vendor Credits'),
+            Icon(Icons.keyboard_arrow_down_outlined, color: Colors.blue)
+          ],
+        ),
+        // actions: [
+        //   Row(
+        //     children: [
+        //       TextButton(
+        //         onPressed: () {},
+        //         style: TextButton.styleFrom(
+        //           backgroundColor: Colors.blue,
+        //           shape: RoundedRectangleBorder(
+        //             borderRadius:
+        //                 BorderRadius.circular(6), // Adjust the radius
+        //           ),
+        //         ),
+        //         child: Row(
+        //           children: [
+        //             Icon(Icons.add, color: Colors.white),
+        //             const Text(
+        //               'NEW',
+        //               style: TextStyle(color: Colors.white),
+        //             ),
+        //           ],
+        //         ),
+        //       ),
+        //     ],
+        //   )
+        // ],
       ),
-      body: const Center(
-        child: Text(
-          'VendorCreditsView is working',
-          style: TextStyle(fontSize: 20),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'You desert some credit too.',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.w900),
+            ),
+            const Text(
+              'Create vendor credits and multiple bills bills when buying stuff from your vendor.',
+            ),
+            TextButton(
+              onPressed: () {},
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.blue,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(6), // Adjust the radius
+                ),
+              ),
+              child: const Text(
+                'CREATE VENDOR CREDITS',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+            const Text(
+              'Import Vendor Credits',
+              style: TextStyle(fontSize: 12, color: Colors.blue),
+            ),
+          ],
         ),
       ),
     );
