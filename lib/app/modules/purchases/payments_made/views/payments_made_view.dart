dd9e722195a1a975c375fc853bde92c11df4167b
DinuchDev <dinuch.sen@gmail.com>
2025-03-18 10:11:53 +0700

    feat(Purchases):all payements

--- lib/app/modules/purchases/payments_made/views/payments_made_view.dart
+++ lib/app/modules/purchases/payments_made/views/payments_made_view.dart
@@ -6,17 +6,73 @@ import '../controllers/payments_made_controller.dart';
 
 class PaymentsMadeView extends GetView<PaymentsMadeController> {
   const PaymentsMadeView({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('PaymentsMadeView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'PaymentsMadeView is working',
-          style: TextStyle(fontSize: 20),
+          automaticallyImplyLeading: false,
+          title: Row(
+            children: [
+              // Text('Shipments'),
+              Text('All Payments'),
+              Icon(Icons.keyboard_arrow_down_outlined, color: Colors.blue)
+            ],
+          ),
+          actions: [
+            Row(
+              children: [
+                TextButton(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                    shape: RoundedRectangleBorder(
+                      borderRadius:
+                          BorderRadius.circular(6), // Adjust the radius
+                    ),
+                  ),
+                  child: Row(
+                    children: [
+                      Icon(Icons.add, color: Colors.white),
+                      const Text(
+                        'NEW',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            )
+          ]),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'You haven\'t made any payments yet.',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.w900),
+            ),
+            const Text(
+              'Receipts of your bill payments wil show up here.',
+            ),
+            TextButton(
+              onPressed: () {},
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.blue,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(6), // Adjust the radius
+                ),
+              ),
+              child: const Text(
+                'CREATE NEW CUSTOMER',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+            const Text(
+              'Import Payments',
+              style: TextStyle(fontSize: 12, color: Colors.blue),
+            ),
+          ],
         ),
       ),
     );
